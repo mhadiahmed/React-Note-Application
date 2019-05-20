078df3d39806343470f96c2ce71fc445455afc74
Mhadi Ahmed <mhadi@localhost.localdomain>
2019-05-20 03:00:22 +0200

    Updates

---
+++ precache-manifest.4c86d0d8aad522d02a9a794889412ce0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df4072bdc56641bccdad26c1b8565d3",
+    "url": "/React-Note-Application/index.html"
+  },
+  {
+    "revision": "e9ee75fd2076062e7c3a",
+    "url": "/React-Note-Application/static/css/main.129ebed6.chunk.css"
+  },
+  {
+    "revision": "18ce5cc12fba4f37f1de",
+    "url": "/React-Note-Application/static/js/2.49efc4e7.chunk.js"
+  },
+  {
+    "revision": "e9ee75fd2076062e7c3a",
+    "url": "/React-Note-Application/static/js/main.0e7041fa.chunk.js"
+  },
+  {
+    "revision": "c780c2ecec2cd05dddb7",
+    "url": "/React-Note-Application/static/js/runtime~main.275286eb.js"
+  }
+]);
